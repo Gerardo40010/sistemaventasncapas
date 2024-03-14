f4ff6ab97639f6c7dbcaee4dd37c7f9a56783c8b
Gerardo Velasquez <gerardodanielvelasquez@gmail.com>
2024-03-14 00:54:38 -0400

    Actualización Base de datos

---
+++ SistemasVentas/SistemasVentas.DAL/ClienteDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class ClienteDal
+    {
+        public DataTable ListarClientesDal()
+        {
+            string consulta = "select * from cliente";
+            DataTable Lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return Lista;
+
+        }
+    }
+}
---
+++ SistemasVentas/SistemasVentas.DAL/DetalleIngDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class DetalleIngDal
+    {
+        public DataTable ListarDetalleIngDal()
+        {
+            string consulta = "select * from Detalleing";
+            DataTable Lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return Lista;
+
+        }
+    }
+}
